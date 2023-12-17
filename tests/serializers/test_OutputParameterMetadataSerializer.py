619734a3dce40321b7e7c64e7c48a9dbf8b1624f
Corey White <ctwhite48@gmail.com>
2023-12-17 13:00:03 -0500

    Added OutputParameter Metadata Serializer and tests

---
+++ tests/serializers/test_OutputParameterMetadataSerializer.py
@@ -0,0 +1,44 @@
+###############################################################################
+# Filename: test_OutputParameterMetadataSerializer.py                          #
+# Project: OpenPlains Inc.                                                     #
+# File Created: Sunday December 17th 2023                                      #
+# Author: Corey White (smortopahri@gmail.com)                                  #
+# Maintainer: Corey White                                                      #
+# -----                                                                        #
+# Last Modified: Sun Dec 17 2023                                               #
+# Modified By: Corey White                                                     #
+# -----                                                                        #
+# License: GPLv3                                                               #
+#                                                                              #
+# Copyright (c) 2023 OpenPlains Inc.                                           #
+#                                                                              #
+# django-actinia is an open-source django app that allows for with             #
+# the Actinia REST API for GRASS GIS for distributed computational tasks.      #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.       #
+#                                                                              #
+###############################################################################
+
+
+from django.test import TestCase
+from grass.serializers.OutputParameterMetadataSerializer import (
+    OutputParameterMetadataSerializer,
+)
+
+
+class OutputParameterMetadataSerializerTest(TestCase):
+    def test_format_field(self):
+        serializer = OutputParameterMetadataSerializer(data={"format": "STAC"})
+        assert serializer.is_valid()
+        assert serializer.validated_data["format"] == "STAC"
