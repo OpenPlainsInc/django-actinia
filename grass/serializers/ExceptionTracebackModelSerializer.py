31569b2a6fc07093f29f17a1b861cdd77c362605
Corey White <ctwhite48@gmail.com>
2023-12-17 13:37:04 -0500

    Added ExpectionTracebackModel  Serializer and tests

---
+++ grass/serializers/ExceptionTracebackModelSerializer.py
@@ -0,0 +1,49 @@
+###############################################################################
+# Filename: ExceptionTracebackModelSerializer.py                               #
+# Project: OpenPlains Inc.                                                     #
+# File Created: Sunday December 17th 2023                                      #
+# Author: Corey White (smortopahri@gmail.com)                                  #
+# Maintainer: Corey White                                                      #
+# -----                                                                        #
+# Last Modified: Sun Dec 17 2023                                               #
+# Modified By: Corey White                                                     #
+# -----                                                                        #
+# License: GPLv3                                                               #
+#                                                                              #
+# Copyright (c) 2023 OpenPlains Inc.                                           #
+#                                                                              #
+# django-actinia is an open-source django app that allows for with             #
+# the Actinia REST API for GRASS GIS for distributed computational tasks.      #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.       #
+#                                                                              #
+###############################################################################
+
+from rest_framework import serializers
+
+
+class ExceptionTracebackModelSerializer(serializers.Serializer):
+    """
+    Serializer for representing exception traceback information.
+    Response schema that contains python3 exception information of the called GRASS GIS module.
+
+    Attributes:
+        message (str): The message that was send with the exception.
+        type (str): The exception type.
+        traceback (list[str]): The exception traceback.
+    """
+
+    message = serializers.CharField()
+    type = serializers.CharField()
+    traceback = serializers.ListField(child=serializers.CharField())
