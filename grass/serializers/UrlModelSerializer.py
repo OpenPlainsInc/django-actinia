cbc50ac65a681e65f85a4a726638274117cd66b4
Corey White <ctwhite48@gmail.com>
2023-12-17 13:41:14 -0500

    Added UrlModel  Serializer and tests

---
+++ grass/serializers/UrlModelSerializer.py
@@ -0,0 +1,48 @@
+###############################################################################
+# Filename: UrlModelSerializer.py                                              #
+# Project: OpenPlains Inc.                                                     #
+# File Created: Sunday December 17th 2023                                      #
+# Author: Corey White (smortopahri@gmail.com)                                  #
+# Maintainer: Corey White                                                      #
+# -----                                                                        #
+# Last Modified: Sun Dec 17 2023                                               #
+# Modified By: Corey White                                                     #
+# -----                                                                        #
+# License: GPLv3                                                               #
+#                                                                              #
+# Copyright (c) 2023 OpenPlains Inc.                                           #
+#                                                                              #
+# django-actinia is an open-source django app that allows for with             #
+# the Actinia REST API for GRASS GIS for distributed computational tasks.      #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.       #
+#                                                                              #
+###############################################################################
+
+from rest_framework import serializers
+
+
+class UrlModelSerializer(serializers.Serializer):
+    """
+    Serializer for Actinia URL schema that points to the status URL
+
+    Attributes:
+        status (str): The URL to check the status of the resource
+        resources (list): A list of URLs to generated resources, that may be GeoTiff files, vector files, ASCII files or png images
+    """
+
+    status = serializers.URLField()  # Add the status URL property
+    resources = serializers.ListField(
+        child=serializers.URLField()
+    )  # Add the resources property
