1a3f15781534d47bbfea0e2e40c8f1ff28aa45d7
Corey White <ctwhite48@gmail.com>
2023-12-17 12:54:09 -0500

    Added ProcessLog Model Serializer

---
+++ grass/serializers/ProcessLogModelSerializer.py
@@ -0,0 +1,59 @@
+###############################################################################
+# Filename: ProcessLogModelSerializer.py                                       #
+# Project: OpenPlains Inc.                                                     #
+# File Created: Sunday December 17th 2023                                      #
+# Author: Corey White (smortopahri@gmail.com)                                  #
+# Maintainer: Corey White                                                      #
+# -----                                                                        #
+# Last Modified: Sun Dec 17 2023                                               #
+# Modified By: Corey White                                                     #
+# -----                                                                        #
+# License: GPLv3                                                               #
+#                                                                              #
+# Copyright (c) 2023 OpenPlains Inc.                                           #
+#                                                                              #
+# django-actinia is an open-source django app that allows for with             #
+# the Actinia REST API for GRASS GIS for distributed computational tasks.      #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.       #
+#                                                                              #
+###############################################################################
+
+
+from rest_framework import serializers
+
+
+class ProcessLogModelSerializer(serializers.Serializer):
+    """
+    Serializer for the ProcessLogModel class.
+
+    Attributes:
+        id (str): The ID of the executable.
+        executable (str): The name of the executable.
+        parameter (list): The parameter of the executable.
+        stdout (str): The stdout output of the executable.
+        stderr (list): The stderr output of the executable as list of strings.
+        return_code (float): The return code of the executable.
+        run_time (float, optional): The runtime of the executable in seconds.
+        mapset_size (float, optional): The size of the mapset in bytes.
+    """
+
+    id = serializers.CharField(required=False)
+    executable = serializers.CharField()
+    parameter = serializers.ListField(child=serializers.CharField())
+    stdout = serializers.CharField()
+    stderr = serializers.ListField(child=serializers.CharField())
+    return_code = serializers.FloatField()
+    run_time = serializers.FloatField(required=False)
+    mapset_size = serializers.FloatField(required=False)
---
+++ tests/serializers/test_ProcessLogModelSerializer.py
@@ -0,0 +1,77 @@
+###############################################################################
+# Filename: test_ProcessLogModelSerializer.py                                  #
+# Project: OpenPlains Inc.                                                     #
+# File Created: Sunday December 17th 2023                                      #
+# Author: Corey White (smortopahri@gmail.com)                                  #
+# Maintainer: Corey White                                                      #
+# -----                                                                        #
+# Last Modified: Sun Dec 17 2023                                               #
+# Modified By: Corey White                                                     #
+# -----                                                                        #
+# License: GPLv3                                                               #
+#                                                                              #
+# Copyright (c) 2023 OpenPlains Inc.                                           #
+#                                                                              #
+# django-actinia is an open-source django app that allows for with             #
+# the Actinia REST API for GRASS GIS for distributed computational tasks.      #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.       #
+#                                                                              #
+###############################################################################
+
+from django.test import TestCase
+from rest_framework import serializers
+from grass.serializers.ProcessLogModelSerializer import ProcessLogModelSerializer
+
+
+class ProcessLogModelSerializerTest(TestCase):
+    def setUp(self):
+        self.serializer = ProcessLogModelSerializer()
+
+    def test_serializer_fields(self):
+        expected_fields = [
+            "id",
+            "executable",
+            "parameter",
+            "stdout",
+            "stderr",
+            "return_code",
+            "run_time",
+            "mapset_size",
+        ]
+        self.assertEqual(set(self.serializer.fields.keys()), set(expected_fields))
+
+    def test_serializer_field_types(self):
+        field_types = {
+            "id": serializers.CharField,
+            "executable": serializers.CharField,
+            "parameter": serializers.ListField,
+            "stdout": serializers.CharField,
+            "stderr": serializers.ListField,
+            "return_code": serializers.FloatField,
+            "run_time": serializers.FloatField,
+            "mapset_size": serializers.FloatField,
+        }
+        for field_name, field_type in field_types.items():
+            self.assertIsInstance(self.serializer.fields[field_name], field_type)
+
+    def test_serializer_required_fields(self):
+        required_fields = ["executable", "parameter", "stdout", "stderr", "return_code"]
+        for field_name in required_fields:
+            self.assertTrue(self.serializer.fields[field_name].required)
+
+    def test_serializer_optional_fields(self):
+        optional_fields = ["id", "run_time", "mapset_size"]
+        for field_name in optional_fields:
+            self.assertFalse(self.serializer.fields[field_name].required)
